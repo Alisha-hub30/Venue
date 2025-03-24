dd341b7e076adf896233193a97fa18c3b547cdc9
[Alisha Shrestha] <alishashrestha240@gmail.com>
2025-03-24 21:53:39 +0545

    Email OTP added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -101,3 +101,35 @@ export const logout = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+// Send Verification OTP to User's Email
+export const sendVerifyOtp = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await userModel.findById(userId);
+    if (user.isAccountVerified) {
+      return res.json({ success: false, message: "Account Already Verified" });
+    }
+    const otp = String(Math.floor(10000 + Math.random() * 900000));
+    user.verifyOtp = otp;
+    user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+    await user.save();
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Account Verification OTP",
+      text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+    };
+    await transporter.sendMail(mailOption);
+    res.json({ success: true, message: "Verification OTP Sent on Email" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { userId, otp } = req.body;
+  if (!userID || !otp) {
+    return res.json({ success: false, message: "Missing Deatails" });
+  }
+};
